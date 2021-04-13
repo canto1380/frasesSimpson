d177b401a970298b49853de99e4e4c8f76370f85
Alejandro Penalba <atapenalba@gmail.com>
2021-04-13 17:29:34 -0300

    Se agregaron los componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Frase from "./components/Frase";
+import { useState, useEffect } from "react";
+import Spinner from './components/Spinner';
 
 function App() {
+  // crear el state
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false)
+
+  useEffect(() => {
+    //  aqui traigo los datos de la api
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async() => {
+
+    setCargando(true)
+    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const resultado = await respuesta.json();
+    console.log(resultado[0]);
+    setTimeout(() => {
+      setCargando(false)
+      setPersonaje(resultado[0]);
+    }, 1500);
+  };
+  let mostrarComponente;
+  // Operador ternario
+  mostrarComponente = (cargando) ? (<Spinner/>) : (<Frase personaje={personaje}/>)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container d-flex flex-column my-5 align-items-center">
+      <img
+        src={process.env.PUBLIC_URL + "logo.png"}
+        alt="logo de los simpsons"
+        className="w-75"
+      />
+      <Button variant="warning" className="my-4 w-75" onClick={() => consultarAPI()}>
+        Obtener frase
+      </Button>
+      {mostrarComponente}
+    </section>
   );
 }
 
---
+++ src/components/Frase.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const Frase = (props) => {
+  return (
+    <Card className='w-100'>
+      <Card.Body>
+        <div className="row">
+          <div className="col-sm-12 col-md-4">
+            <img src={props.personaje.image} alt={props.personaje.character} />
+          </div>
+          <div className="col-sm-12 col-md-8">
+            <Card.Title>{props.personaje.character}</Card.Title>
+            <Card.Text>{props.personaje.quote}</Card.Text>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Frase;
